51e99a75c788c54f3c78bec7e52b6800eb17b7e4
Lucas Simpson <lucassimpson05@gmail.com>
2015-11-17 22:23:55 -0500

    updated deep_MNIST with tensorboard stats :)

--- mine/deep_MNIST/run.py
+++ mine/deep_MNIST/run.py
@@ -36,7 +36,8 @@ def cross_entropy_layer (prev_layer, comparison_logits):
 # creates a convolution layer
 # PADDING: SAME -> only full sized windows vs VALID -> partial windows allowed
 def conv2d_layer (prev_layer, kernel_width, kernel_height, stride_x, stride_y, num_kernels, padding='SAME'):
-	filter_ = weight_variable ([kernel_height, kernel_width, prev_layer.get_shape ().as_list () [3], num_kernels])
+	z_depth = prev_layer.get_shape ().as_list () [3]
+	filter_ = weight_variable ([kernel_height, kernel_width, z_depth, num_kernels])
 
 	return tf.nn.conv2d (
 		input = prev_layer, 
@@ -57,7 +58,6 @@ def max_pool_layer (prev_layer, pool_width, pool_height, stride_x, stride_y, pad
 
 # creates a rectified linear layer
 def relu_layer (prev_layer):
-	print prev_layer
 	return tf.nn.relu (prev_layer)
 
 # creates a dropout layer
@@ -65,11 +65,14 @@ def dropout_layer (prev_layer, keep_prob):
 	return tf.nn.dropout (prev_layer, keep_prob)
 
 # some params
-num_tests = 1000
+num_tests = 400
 batch_size = 50
 
+# start interactive session
+sess = tf.InteractiveSession ()
+
 # define data source
-mnist = input_data.read_data_sets ("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets ("/tmp/MNIST_data/", one_hot=True)
 
 # define variables
 inputs = tf.placeholder ("float", shape=[None, 28 * 28])
@@ -81,6 +84,7 @@ grid_inputs = tf.reshape (inputs, [-1, 28, 28, 1])
 
 # first layer
 conv1 = conv2d_layer (grid_inputs, 5, 5, 1, 1, 32)
+print conv1
 relu1 = relu_layer (conv1)
 mxp1 = max_pool_layer (relu1, 2, 2, 2, 2)
 
@@ -109,22 +113,30 @@ train_step = tf.train.AdamOptimizer (1e-4).minimize (cross_entropy)
 is_right = tf.equal (tf.argmax (outputs, 1), tf.argmax (actual, 1))
 accuracy = tf.reduce_mean (tf.cast (is_right, "float"))
 
-# train
+# loggin and visualization
+accuracy_sum = tf.scalar_summary ("Accuracy", accuracy)
+cross_entropy_hist = tf.scalar_summary ("Cross Entropy", cross_entropy)
+conv1_k1 = tf.image_summary ("Conv1_k1", tf.split (3, 32, conv1) [0])
+
+merged_summary_op = tf.merge_all_summaries ()
+writer = tf.train.SummaryWriter ("./logs", accuracy.graph.as_graph_def ())
+
+# train setup
 init = tf.initialize_all_variables ()
-sess = tf.Session ()
 sess.run (init)
 
-
-for a in range (num_tests):
+# training
+for a in range (num_tests + 1):
 	batch = mnist.train.next_batch (batch_size)
 
-	if a % 100 == 0:
-		acc = sess.run (accuracy, feed_dict={
+	if a % 25 == 0:
+		summary, acc = sess.run ([merged_summary_op, accuracy], feed_dict={
 				inputs: batch [0],
 				actual: batch [1],
 				keep_prob: 1.0,
 			})
 		print '%s / %s complete. Accuracy=%s' % (a, num_tests, acc)
+		writer.add_summary (summary, a)
 
 	feed_dict = {
 		inputs: batch [0],
